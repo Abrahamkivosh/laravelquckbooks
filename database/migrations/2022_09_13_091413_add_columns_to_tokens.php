a1632827378736644a424db606104ba0e1074fff
migo1 <robertmigot08@gmail.com>
2022-09-13 13:01:48 +0300

    debug

---
+++ database/migrations/2022_09_13_091413_add_columns_to_tokens.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tokens', function (Blueprint $table) {
+            $table->after('qb_token', function($table){
+                $table->string('company_id')->nullable();
+                $table->string('refresh_token')->nullable();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tokens', function (Blueprint $table) {
+            $table->dropColumn(['company_id', 'refresh_token']);
+        });
+    }
+};
